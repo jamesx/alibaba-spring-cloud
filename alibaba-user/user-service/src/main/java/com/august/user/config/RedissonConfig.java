07901b869f9ab8f318135d34ecc6a4b3b7778359
jamesx <379249906@qq.com>
2020-04-05 11:35:04 +0800

    redission路径写死了

--- alibaba-user/user-service/src/main/java/com/august/user/config/RedissonConfig.java
+++ alibaba-user/user-service/src/main/java/com/august/user/config/RedissonConfig.java
@@ -3,16 +3,23 @@ package com.august.user.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
 	@Bean
 	public RedissonClient redissonClient() {
+
+		System.out.println("redisHost: "+redisHost);
+
 		Config config = new Config();
-		config.useSingleServer().setAddress("redis://localhost:6379");
+		config.useSingleServer().setAddress("redis://" +redisHost+ ":6379");
 		return Redisson.create(config);
 	}
 }
